c728bca00d7075aa5e7d780f7c6ff0360e1941e6
Andrea Mantovani <andreamnt94@gmail.com>
2018-06-21 21:09:51 +0200

    Removed unnecessary logs

--- src/bootstrap.cpp
+++ src/bootstrap.cpp
@@ -3,29 +3,26 @@
 #include <csignal>
 #include <cstdlib>
 #include <iostream>
-#include <vector>
 
 #define ENV_DHT_NODE_PORT	"DHT_NODE_PORT"
 #define ERR_DHT_PORT_NSET	"The env variable DHT_NODE_PORT is not set in the current system."
 #define MSG_ABORT			"Abort!"
-#define MSG_BOOSTRAP_CLOSE	"Bootstrapper node will be close down..."
-#define MSG_BOOSTRAP_START	"Starting boostrapper node..."
-#define MSG_DONE			"Ok!"
-#define MSG_LISTENING		"Boostrapper node is listening on port"
+#define MSG_BOOSTRAP_CLOSE	"Bootstrap node will be close down..."
+#define MSG_LISTENING		"Bootstrap node is listening on port"
 #define MSG_SIGTERM			"SIGTERM received!"
-#define MSG_TERMINATED		"Boostrapper node terminated!"
+#define MSG_TERMINATED		"Bootstrap node terminated!"
 
-static bool waitTermination = true;
+static bool __waitTermination = true;
 
-void StopBoostrapperNode(int sign) {
+void StopBootstrapNode(int sign) {
 	std::cout << MSG_SIGTERM << " " << MSG_BOOSTRAP_CLOSE << std::endl;
-	waitTermination = false;
+	__waitTermination = false;
 }
 
 int main()
 {
 	// Initialize signal handler
-	std::signal(SIGTERM, StopBoostrapperNode);
+	std::signal(SIGTERM, StopBootstrapNode);
 
 	const char *envPort = std::getenv(ENV_DHT_NODE_PORT);
 
@@ -42,8 +39,8 @@ int main()
 	// generated RSA key pair, and listen on port set by the env variable DHT_NODE_PORT.
 	node.run(port, dht::crypto::generateIdentity(), true);
 	std::cout << MSG_LISTENING << " " << port << "..." << std::endl;
-
-	while (waitTermination);
+	// Wait until a SIGTERM is emitted
+	while (__waitTermination);
 
 	// Wait for dht threads to end
 	node.join();
