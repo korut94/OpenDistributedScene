cc1630dc0ea197d6560dd9942b7eb78c2419fd28
Andrea Mantovani <andreamnt94@gmail.com>
2018-06-29 15:05:15 +0200

    Updated termination waiting phase by condition_variable

--- tools/bootstrap.cpp
+++ tools/bootstrap.cpp
@@ -1,22 +1,27 @@
 #include <opendht.h>
 
+#include <condition_variable>
 #include <csignal>
 #include <cstdlib>
 #include <iostream>
+#include <mutex>
 
 #define ENV_DHT_NODE_PORT	"DHT_NODE_PORT"
-#define ERR_DHT_PORT_NSET	"The env variable DHT_NODE_PORT is not set in the current system."
-#define MSG_ABORT			"Abort!"
-#define MSG_BOOSTRAP_CLOSE	"Bootstrap node will be close down..."
-#define MSG_LISTENING		"Bootstrap node is listening on port"
-#define MSG_SIGTERM			"SIGTERM received!"
-#define MSG_TERMINATED		"Bootstrap node terminated!"
 
-static bool __waitTermination = true;
+dht::DhtRunner node;
+
+std::condition_variable cv;
+std::mutex m;
+std::atomic_bool done {false};
 
 void StopBootstrapNode(int sign) {
-	std::cout << MSG_SIGTERM << " " << MSG_BOOSTRAP_CLOSE << std::endl;
-	__waitTermination = false;
+	std::cout << "SIGTERM received! Waiting shutdown... ";
+	node.shutdown([]{
+		std::cout << "Done" << std::endl; 
+        std::lock_guard<std::mutex> lk(m);
+        done = true;
+        cv.notify_one();
+    });
 }
 
 int main()
@@ -27,24 +32,24 @@ int main()
 	const char *envPort = std::getenv(ENV_DHT_NODE_PORT);
 
 	if (envPort == nullptr) {
-		std::cerr << ERR_DHT_PORT_NSET << " " << MSG_ABORT << std::endl;
+		std::cerr << "The env variable DHT_NODE_PORT is not set in the current system. Abort!" << std::endl;
 		return 1;
 	}
 
 	uint port = std::atoi(envPort);
 
-	dht::DhtRunner node;
-
 	// Launch the dht node on a new thread, using a
 	// generated RSA key pair, and listen on port set by the env variable DHT_NODE_PORT.
 	node.run(port, dht::crypto::generateIdentity(), true);
-	std::cout << MSG_LISTENING << " " << port << "..." << std::endl;
-	// Wait until a SIGTERM is emitted
-	while (__waitTermination);
+	std::cout << "Bootstrap node is listening on port " << port << "..." << std::endl;
+	
+    // Wait for shutdown
+    std::unique_lock<std::mutex> lk(m);
+    cv.wait(lk, []{ return done.load(); });
 
-	// Wait for dht threads to end
+	// Wait dht thread ending
 	node.join();
-	std::cout << MSG_TERMINATED << std::endl;
+	std::cout << "Bootstrap node terminated!" << std::endl;
 
 	return 0;
 }
