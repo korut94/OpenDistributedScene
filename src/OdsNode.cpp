4e5902c54a66dbc8f65bad441552833f6dc505f5
Andrea Mantovani <andreamnt94@gmail.com>
2018-08-09 17:41:36 +0200

    Update

--- src/OdsNode.cpp
+++ src/OdsNode.cpp
@@ -1,7 +1,6 @@
 #include <opendht.h>
 
 #include <iostream>
-#include <map>
 #include <string>
 
 #define DEFAULT_HOST_PORT 4222
@@ -11,32 +10,53 @@
 extern "C"
 {
 	bool PlayerTableUpdate(const std::vector<std::shared_ptr<dht::Value>>& values, bool expired);
-	bool PlayerUpdate(const std::vector<std::shared_ptr<dht::Value>>& attributes);
 
 	typedef int (*OnConnectionStatusChanged)(dht::NodeStatus, dht::NodeStatus);
-	typedef int (*OnDone)(bool);
+	typedef int (*OnDone)(bool done);
 	typedef int (*OnPlayerLeft)(const char *id);
 	typedef int (*OnPlayerJoin)(const char *id);
+	typedef int (*OnValueChanged)(const char *entity, uint id, const char *value);
 
 	dht::DhtRunner _node;
-	std::map<dht::InfoHash, std::future<size_t>> _playersToken;
+	std::vector<dht::InfoHash> _playersHash;
 
 	OnPlayerLeft	_playerLeftHandler = nullptr;
 	OnPlayerJoin	_playerJoinHandler = nullptr;
+	OnValueChanged	_valueChangedHandler = nullptr;
 
 	std::future<size_t> _playersTableToken;
 
 	void DHTNodeBootstrap(const char *host, const char *service, OnConnectionStatusChanged callback) {
 		std::cout << "Bootstrapping to '" << host << ":" << service << "'" << std::endl;
 		_node.bootstrap(host, service);
+
+		// Prefetch all the players already connected
+		_node.get(dht::InfoHash::get(PLAYER_TABLE_KEY), [](const std::vector<std::shared_ptr<dht::Value>> &values) {
+			PlayerTableUpdate(values, false);
+			return true;
+		}, [=](bool done) {});
 	}
 
-	void DHTNodePutValue(const char *netId, uint id, const char *serializedValue, int size) {
+	void DHTNodeFetchPlayersUpdate() {
+		for (const auto &player : _playersHash) {
+			_node.get(player, [&player](const std::vector<std::shared_ptr<dht::Value>>& attributes) {
+				for (const auto& attr : attributes) {
+					if (_valueChangedHandler != nullptr) {
+						_valueChangedHandler(player.to_c_str(), attr->id, dht::Value::unpack<std::string>(*attr).c_str());
+					}
+				}
+				return true;
+			}, [](bool success) {});
+		}
+	}
+
+	void DHTNodePutValue(const char *netId, uint id, const char *serializedValue) {
 		std::cout << "Put operation on: " << netId << " " << id << " " << serializedValue << std::endl;
 
 		(void) netId; // For future works
 
-		dht::Value value((const uint8_t*)serializedValue, size);
+		std::string buffer(serializedValue);
+		dht::Value value(buffer);
 		// Replace the old existing value over the DHT
 		value.id = id;
 
@@ -57,6 +77,10 @@ extern "C"
 		_playerJoinHandler = handler;
 	}
 
+	void DHTNodeSetOnValueChanged(OnValueChanged handler) {
+		_valueChangedHandler = handler;
+	}
+
 	void DHTNodeSetOnStatusChanged(OnConnectionStatusChanged handler) {
 		_node.setOnStatusChanged([handler](dht::NodeStatus ipv4, dht::NodeStatus ipv6) {
 			std::cout << "Connection status ipv4 changed to: " << ((ipv4 == dht::NodeStatus::Connected) ? "Connected" : "Disconnected") << std::endl;
@@ -67,37 +91,43 @@ extern "C"
 
 	void DHTNodeSubscribePlayer(OnDone callback) {
 		auto playersTableHash = dht::InfoHash::get(PLAYER_TABLE_KEY);
-
-		_playersTableToken = _node.listen(playersTableHash, PlayerTableUpdate);
-		std::cout << "Starting listening '" << PLAYER_TABLE_KEY << "' table to new incoming players connections" << std::endl;
-		
-		std::cout << "Subscribing node '" << _node.getNodeId() << "' to '" << PLAYER_TABLE_KEY << "' table" << std::endl;
-		// Require manual update subscription to avoid expiring
-		_node.put(playersTableHash, _node.getNodeId(), callback);
+	
+		std::cout << "Subscribing node '" << _node.getNodeId() << "' to '" << PLAYER_TABLE_KEY << "' table..." << std::endl;
+		_node.put(playersTableHash, _node.getNodeId(), [callback, playersTableHash](bool done) {
+			std::cout << "Starting listening '" << PLAYER_TABLE_KEY << "' table to new incoming players connections" << std::endl;
+			_playersTableToken = _node.listen(playersTableHash, PlayerTableUpdate);
+			callback(done);
+		});
 	}
 
 	bool PlayerTableUpdate(const std::vector<std::shared_ptr<dht::Value>> &values, bool expired) {
 		for (const auto& v : values) {
-			dht::InfoHash hash = dht::Value::unpack<dht::InfoHash>(*v);
+			dht::InfoHash player = dht::Value::unpack<dht::InfoHash>(*v);
 			// A different player is joined or expired to the network
-			if (hash != _node.getNodeId()) {
+			if (player != _node.getNodeId()) {
 				if (expired) {
-					if (_playerLeftHandler != nullptr) { _playerLeftHandler(hash.to_c_str()); }
+					std::cout << "Player " << player << "is expired!" << std::endl;
+					if (_playerLeftHandler != nullptr) { _playerLeftHandler(player.to_c_str()); }
 				} else {
-					// Start to listen the key player 
-					_playersToken[hash] = _node.listen(hash, PlayerUpdate);
-					std::cout << "Starting listening player '" << hash << "'" << std::endl;
-					if (_playerJoinHandler != nullptr) { _playerJoinHandler(hash.to_c_str()); }
+					if (_playerJoinHandler != nullptr) { _playerJoinHandler(player.to_c_str()); }
+					std::cout << "Listening player " << player << std::endl;
+					// Keeping track of the new player
+					// _playersHash.push_back(hash);
+					_node.listen(player, [=](const std::vector<std::shared_ptr<dht::Value>>& attributes) {
+						for (const auto& attr : attributes) {
+							std::cout << "Attribute " << attr->id << " = " << dht::Value::unpack<std::string>(*attr) << std::endl;
+
+							if (_valueChangedHandler != nullptr) {
+								_valueChangedHandler(player.to_c_str(), attr->id, dht::Value::unpack<std::string>(*attr).c_str());
+							}
+						}
+
+						return true;
+					});
 				}
 			}
-
-            std::cout << "Player id: " << dht::Value::unpack<dht::InfoHash>(*v) << (expired ? " [expired]" : "") << std::endl;
 		}
 
         return true;
-	}
-
-	bool PlayerUpdate(const std::vector<std::shared_ptr<dht::Value>>& attributes) {
-		return true;
 	}
 }
